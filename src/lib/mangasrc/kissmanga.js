df171ef9e0f448a4eeb104e1bdcebaebef9af38d
sasalx <oyundanben@gmail.com>
2017-12-06 00:17:23 +0300

    Grey out button

--- src/lib/mangasrc/kissmanga.js
+++ src/lib/mangasrc/kissmanga.js
@@ -4,14 +4,12 @@ const hakuneko = require('hakuneko');
 	regex=(.*\.jpe?g|.*\.png)
 	its for the handling urls correction
 
-
-
  */
 
 function buttoKun(){
 	manga = hakuneko.base.createManga( 'Title', '/Manga/Bleach' );
-	hakuneko.kissmanga.getChapters( manga, function( error, chapters ) {
-		if( !error ) {
+	hakuneko.kissmanga.getChapters( manga, function( error, chapters ){
+		if(!error){
 
 			chapter = hakuneko.base.createChapter('[VOL]', '[NR]', 'Title', 'lang', 'scanlator',
 				'/Manga/Bleach', []);
@@ -29,3 +27,24 @@ function buttoKun(){
 
 	});
 }
+
+// Grey out the button if at least one input is missing
+(function(){
+    $("form input").on("keyup change",function() {
+
+        let empty = false;
+        $('form input').each(function() {
+			// Check is any input value is empty
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if(empty){
+            $('#butto-kun').attr('disabled', 'disabled'); 
+		} 
+		else{
+            $('#butto-kun').removeAttr('disabled');
+        }
+    });
+})()
