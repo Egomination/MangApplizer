710dbd9a152c06b0c1d47f52626c747f091160fc
Taylan Dogan <kondanta@users.noreply.github.com>
2017-12-08 12:59:02 +0300

    Downloading from kissmanga accomplished!

--- src/lib/mangasrc/kissmanga.js
+++ src/lib/mangasrc/kissmanga.js
@@ -1,4 +1,5 @@
-const hakuneko = require('hakuneko');
+const hakuneko = require('hakuneko'),
+    https = require('https');
 
 /*
 	regex=(.*\.jpe?g|.*\.png)
@@ -16,8 +17,8 @@ function test(){
 
 function buttoKun() {
     // TODO: nmanga -> REGEX INCOMING!!!!!
-    // FIXME:foldername = foldername.toLowerCase() foldername = foldername.replace(/ /g, "-");
-    // chapterNo = chapters.length - chno;
+    // TODO: Add a path for download. Also don't redefine the requiremens on lhscans
+    // these are still valid on this page. bkz: fs.createWriteStream
     let nmanga = document.getElementById('fname').value;
     let chno = document.getElementById('chno').value;
 
@@ -28,13 +29,30 @@ function buttoKun() {
             chapter = hakuneko.base.createChapter('[VOL]', '[NR]', 'Title',
                 'lang', 'scanlator', `/Manga/${nmanga}`, []);
             chapterNo = (chapters.length - chno) - 1; // Because of array
+            console.log(chapterNo);
             chapter = chapters[chapterNo];
-
+            console.log(chapter);
             hakuneko.kissmanga.getPages(chapter, function(error, pages) {
                 if (!error) {
-                    chapter.p = pages; // assign pages to chapter
+                    pages.forEach(function(item){
+                        item = item.trim();
+                        let regex = new RegExp(/(.*\.jpe?g|.*\.png)/)
+                        item = item.match(regex); // FIXME: regex returns to array.
+
+                        // Downloading from Https, doesnt work
+                        // So i had to convert it to http
+                        let link = item[0].split('://');
+                        let newLink = "http://"+link[1];
+                        
+                        let file = fs.createWriteStream('./imgs/' +
+                            newLink.split('-').pop(-1));
+                        let req = http.get(newLink, function(response) {
+                            response.pipe(file)
+                        });
+                    });
+                    // chapter.p = pages; // assign pages to chapter
                 }
-                console.log(error, pages)
+                // console.log(error, pages)
             });
         } else {
             console.log(error);
