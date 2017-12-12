4a7bacaf93898b744e25239eb6bc3e4c1abab80c
Taylan Dogan <kondanta@users.noreply.github.com>
2017-12-12 15:52:08 +0300

    Chapter no input error handling if user gives greater number than available chapters, chapter becomes the last one automatically.

--- src/lib/mangasrc/kissmanga.js
+++ src/lib/mangasrc/kissmanga.js
@@ -11,8 +11,6 @@ function buttoKun() {
     nmanga = nmanga.toLowerCase()
     nmanga = nmanga.replace(/ /g, "-");
 
-    // let path = './imgs/' + nmanga + '/' + chno + '/';
-
     manga = hakuneko.base.createManga('Title', `/Manga/${nmanga}`);
     hakuneko.kissmanga.getChapters(manga, function(error, chapters) {
         if (!error) {
@@ -20,9 +18,16 @@ function buttoKun() {
             chapter = hakuneko.base.createChapter('[VOL]', '[NR]', 'Title',
                 'lang', 'scanlator', `/Manga/${nmanga}`, []);
             chapterNo = (chapters.length - chno) - 1; // Because of array
-            chapter = chapters[chapterNo];
+
+            // Handling the chapter no input greater than number of chaps.
+            if (chapterNo > chapters.length || chapterNo <= 0) {
+                chapter = chapters[0];
+            } else {
+                chapter = chapters[chapterNo];
+            }
             // Creating the chapter name with actual ch number + title if exists.
             let path = './imgs/' + nmanga + '/' + chapter.t + '/';
+
             mkdirp(path)
                 .catch(console.error);
 
