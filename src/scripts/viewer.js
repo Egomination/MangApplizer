81659d2c119862b5f411c31eecc094541dabb879
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2017-12-30 18:00:36 +0300

    Merging!

--- src/scripts/viewer.js
+++ src/scripts/viewer.js
@@ -8,11 +8,7 @@ let arr = [];
 // Needed for Materialize Design
 $(document).ready(function() {
     $('select').material_select();
-<<<<<<< HEAD
-    $('.carousel.carousel-slider').carousel({ fullWidth: true });
-=======
     $('.carousel.carousel-slider').carousel({fullWidth: true, noWrap: true});
->>>>>>> 4199b2506b551320a6857212fef2459541e9262f
 });
 
 // Sorting function for an array of numbers
@@ -23,12 +19,6 @@ function sortNumber(a,b) {
 // Opening event of viewer window
 ipcRenderer.on('open-viewer-reply', (event, mangaPath) => {
     path = mangaPath;
-<<<<<<< HEAD
-    // NOTE: Add this to the html file
-    $("#chapterlist")
-        .append('<option value="nul" disabled selected>Please select a chapter</option>');
-=======
->>>>>>> 4199b2506b551320a6857212fef2459541e9262f
     // Folder listing for viewer window
     fs.readdirSync(mangaPath).forEach(file => {
         arr.push(file);
@@ -38,11 +28,7 @@ ipcRenderer.on('open-viewer-reply', (event, mangaPath) => {
     console.log("arr2:" + arr);
     arr.forEach(item => {
         $("#chapterlist").append(
-<<<<<<< HEAD
-            $("<option>").attr("value", `${file}`).append(`${file}`)
-=======
             $("<option>").attr("value", `${item}`).append(`${item}`)
->>>>>>> 4199b2506b551320a6857212fef2459541e9262f
         );
     });
     arr = [];
