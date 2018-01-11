74c91d86842243c72943117e3d9bcf6c220cf6b8
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-01-11 18:16:52 +0300

    Prettify!

--- src/jsoner.js
+++ src/jsoner.js
@@ -1,18 +1,17 @@
 const fs = require("fs");
 
-function createDb(value){
-	fs.writeFileSync("db.json", value);
+function createDb(value) {
+    fs.writeFileSync("db.json", value);
 }
 
-module.exports = function (keys, value){
-	let obj = {};
-	let key = keys;
-	for(i = 0; i<keys.length; i++){
-		obj[key[i]] = value[i];
-	}
+module.exports = function(keys, value) {
+    let obj = {};
+    let key = keys;
+    for (i = 0; i < keys.length; i++) {
+        obj[key[i]] = value[i];
+    }
 
-	let wrable = JSON.stringify(obj); 
-	console.log(wrable);
-	createDb(wrable);
+    let wrable = JSON.stringify(obj);
+    console.log(wrable);
+    createDb(wrable);
 }
-
--- src/rsc/lhs.js
+++ src/rsc/lhs.js
@@ -3,58 +3,58 @@ const cheerio = require("cheerio");
 const jso = require("../jsoner");
 const fs = require("fs");
 
-class LHS{
-	constructor(title){
-		this.title = title;
-		this.existingChapters = [];
-		this.existingManga = {};
-		this.BASE_URL = "http://lhscans.com/";
-	}
+class LHS {
+    constructor(title) {
+        this.title = title;
+        this.existingChapters = [];
+        this.existingManga = {};
+        this.BASE_URL = "http://lhscans.com/";
+    }
 
-	get(url, callback){
-		request(url, function(error, response, body){
-			callback(response, body);
-		});
-	}
+    get(url, callback) {
+        request(url, function(error, response, body) {
+            callback(response, body);
+        });
+    }
 
-	/**
-	 * 
-	 */
-	getAllManga(url){
-		// all pages -> available mangas in lhs
-		let value = [];
-		let keys = [];
-		url = this.BASE_URL + "manga-list.html?listType=allABC";
-		this.get(url, function(response, body){
-			if(response.statusCode !== 200) return;
-			const $ = cheerio.load(body);
-			let info = $(body).find("span a").each(function(index, element){
-				let key = $(element).text();
-				let val = $(element).attr("href");
-				keys.push(key);
-				value.push(val);
-			});
-			jso(keys, value);
-		});
-	}
+    /**
+     *
+     */
+    getAllManga(url) {
+        // all pages -> available mangas in lhs
+        let value = [];
+        let keys = [];
+        url = this.BASE_URL + "manga-list.html?listType=allABC";
+        this.get(url, function(response, body) {
+            if (response.statusCode !== 200) return;
+            const $ = cheerio.load(body);
+            let info = $(body).find("span a").each(function(index, element) {
+                let key = $(element).text();
+                let val = $(element).attr("href");
+                keys.push(key);
+                value.push(val);
+            });
+            jso(keys, value);
+        });
+    }
 
-	getAvailableManga(callback){
-		fs.readFile("db.json", (error, data) => {
-			if(error) throw error;
-			let value = JSON.parse(data);
-			// console.log(value);
-			callback(null, value);
-		});
-	}
+    getAvailableManga(callback) {
+        fs.readFile("db.json", (error, data) => {
+            if (error) throw error;
+            let value = JSON.parse(data);
+            // console.log(value);
+            callback(null, value);
+        });
+    }
 
-	getChapters(name){
-		name = name + " - Raw";
-		let mangaUrl;
-		this.getAvailableManga((error, data) => {
-			mangaUrl = data[name];
-			console.log(mangaUrl);
-		});
-	}
+    getChapters(name) {
+        name = name + " - Raw";
+        let mangaUrl;
+        this.getAvailableManga((error, data) => {
+            mangaUrl = data[name];
+            console.log(mangaUrl);
+        });
+    }
 }
 
 let obj = new LHS('test');
