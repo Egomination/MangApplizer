a8ec75ae3c4e8a27b10810a2ea2fdbbec926f280
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-01-12 19:20:12 +0300

    Add downloader

---
+++ src/downloader.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const fs = require("fs");
+
+// TODO: Add chNo
+// TODO: Fix enoent,no folder available, issue wth creating path beforehand.
+module.exports = function(url, name /*,path*/ ) {
+    let file = fs.createWriteStream("../../imgs/" + name + "/" + url.split("/").pop(-1)
+    	.toLowerCase());
+    let download = http.get(url, function(response) {
+        response.pipe(file);
+    });
+}
