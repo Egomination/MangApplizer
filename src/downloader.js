d47302eb565853339bc84037b978228cb0be664e
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-01-14 20:06:02 +0300

    Add createFolder

--- src/downloader.js
+++ src/downloader.js
@@ -1,12 +1,29 @@
 const http = require("http");
 const fs = require("fs");
+const mkdirp = require("mkdirp-promise");
 
-// TODO: Add chNo
-// TODO: Fix enoent,no folder available, issue wth creating path beforehand.
-module.exports = function(url, name /*,path*/ ) {
-    let file = fs.createWriteStream("../../imgs/" + name + "/" + url.split("/").pop(-1)
-        .toLowerCase());
-    let download = http.get(url, function(response) {
+function createFolders(name, chNo) {
+    let filePath = "../../imgs/" + name + "/" + chNo + "/";
+    mkdirp(filePath)
+        .then(console.log)
+        /*
+            Returns path if it's created -> workEnvironment/projects/development/Elec/imgs/G Men - Raw
+            returns null, if path is already exist.
+        */
+        .catch(console.error);
+    return filePath;
+}
+
+function downloader(url, path) {
+    let chapters = url.split("/").pop(-1).toLowerCase();
+    let chapNumbers = path + chapters;
+    let file = fs.createWriteStream(chapNumbers);
+    http.get(url, function(response) {
         response.pipe(file);
     });
+}
+
+module.exports = {
+    downloader: downloader,
+    createFolders: createFolders
 };
