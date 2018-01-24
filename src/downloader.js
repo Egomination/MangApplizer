0280213ac12c2c07d4a8094bd707e173d944479e
Taylan Dogan <16501501+kondanta@users.noreply.github.com>
2018-01-24 19:13:01 +0300

    let to const conversion

--- src/downloader.js
+++ src/downloader.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const mkdirp = require("mkdirp-promise");
 
 function createFolders(name, chNo) {
-    let filePath = "../../imgs/" + name + "/" + chNo + "/";
+    const filePath = "../../imgs/" + name + "/" + chNo + "/";
     mkdirp(filePath)
         .then(console.log)
         /*
@@ -16,15 +16,15 @@ function createFolders(name, chNo) {
 
 module.exports = function(url, name, chNo) {
     url = url.filter((i) => i);
-    let path = createFolders(name, chNo);
+    const path = createFolders(name, chNo);
     // Waiting folder creation
     setTimeout(function() {
         url.forEach(function(item) {
             item = item.trim();
             console.log(item);
-            let chapters = item.split("/").pop(-1).toLowerCase();
-            let chapNumbers = path + chapters;
-            let file = fs.createWriteStream(chapNumbers);
+            const chapters = item.split("/").pop(-1).toLowerCase();
+            const chapNumbers = path + chapters;
+            const file = fs.createWriteStream(chapNumbers);
             http.get(item, function(response) {
                 response.pipe(file);
             });
